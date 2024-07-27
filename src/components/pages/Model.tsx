4f9282a2bd9d31fdf5b4ee4d24308a262af8e1a8
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-07-27 21:08:10 +0300

    Новая компонент Model

---
+++ src/components/pages/Model.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {useParams} from "react-router-dom";
+import {adidasArr} from "./Adidas";
+
+export const Model = () => {
+    const params = useParams()
+    console.log(params)
+    return (
+        <div style={{textAlign: "center"}}>
+            <h2>{adidasArr[Number(params.id) - 1].model}</h2>
+            <h4>{adidasArr[Number(params.id) - 1].collection}</h4>
+            <h4>{adidasArr[Number(params.id) - 1].price}</h4>
+            <img src={adidasArr[Number(params.id) - 1].picture}
+                 style={{width: '300', height: 'auto', marginRight: '10px'}}
+                 alt={''}/>
+        </div>
+    );
+};
+
