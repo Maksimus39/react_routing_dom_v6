944d00b759bf29c49b43f5dfcf6bbdf19e011c8c
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-07-31 21:55:44 +0300

    Переписал на метод find

--- src/components/pages/ModelPuma.tsx
+++ src/components/pages/ModelPuma.tsx
@@ -4,15 +4,22 @@ import {PumaArr} from "./Puma";
 
 export const ModelPuma = () => {
     const params = useParams()
-    console.log(params)
+
+    const currentModel = PumaArr.find(el => el.id === Number(params.id))
+
     return (
         <div style={{textAlign: "center"}}>
-            <h2>{PumaArr[Number(params.id) - 1].model}</h2>
-            <h4>{PumaArr[Number(params.id) - 1].collection}</h4>
-            <h4>{PumaArr[Number(params.id) - 1].price}</h4>
-            <img src={PumaArr[Number(params.id) - 1].picture}
-                 style={{width: '300', height: 'auto', marginRight: '10px'}}
-                 alt={''}/>
+            {currentModel
+                ? <>
+                    <h2>{PumaArr[Number(params.id)].model}</h2>
+                    <h4>{PumaArr[Number(params.id)].collection}</h4>
+                    <h4>{PumaArr[Number(params.id)].price}</h4>
+                    <img src={PumaArr[Number(params.id)].picture}
+                         style={{width: '300', height: 'auto', marginRight: '10px'}}
+                         alt={''}/>
+                </>
+                : <h2>Такой модели не существует</h2>
+            }
         </div>
     );
 };
